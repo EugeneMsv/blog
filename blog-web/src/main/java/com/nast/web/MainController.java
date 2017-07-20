0e2c805606c134cdcb13cc81bf7b36f9cdbb6a88
Eugene_Moiseev <msv.eug@gmail.com>
2017-07-20 11:43:52 +0300

    general edits and react is alive

--- blog-web/src/main/java/com/nast/web/MainController.java
+++ blog-web/src/main/java/com/nast/web/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
-    @LoggableCall(logLevel = LogLevel.ERROR, showArgs = true, timeRecord = true, showOutput = true)
+    @LoggableCall(logLevel = LogLevel.DEBUG)
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String index() {
         System.out.println("MainController.index");
